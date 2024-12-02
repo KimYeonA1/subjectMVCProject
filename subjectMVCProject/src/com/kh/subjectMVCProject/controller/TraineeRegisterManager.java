8f5bcfdecc93f2e97f68c1a640772134125eedcb
KimYeonA1 <85yuna@naver.com>
2024-12-03 01:02:45 +0900

    TraineeDAO 수정 완료

--- subjectMVCProject/src/com/kh/subjectMVCProject/controller/TraineeRegisterManager.java
+++ subjectMVCProject/src/com/kh/subjectMVCProject/controller/TraineeRegisterManager.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 import com.kh.subjectMVCProject.model.TraineeVO;
 
 
-
 public class TraineeRegisterManager {
 	public static Scanner sc = new Scanner(System.in); 
 	//전체 리스트를 출력요청(조인)
@@ -37,9 +36,6 @@ public class TraineeRegisterManager {
 
 	public static void insertManager() {
 		TraineeDAO tdao = new TraineeDAO(); 
-		// 3.statement
-		//student 검색기능추가
-		//검색된 이름으로 학번, 이름, 이메일 출력 통해서 학번입력처리
 		StudentRegisterManager srm = new StudentRegisterManager(); 
 		srm.selectNameSearchManager();
 		System.out.print("학생번호등록>>");
@@ -69,7 +65,6 @@ public class TraineeRegisterManager {
 		
 		//Trainee 테이블 전체내용을 보여준다. 
 		selectManager();
-		// 3.statement
 		//student 검색기능추가
 		System.out.print("수정할 번호 입력하세요>>");
 		int no = Integer.parseInt(sc.nextLine());
@@ -79,7 +74,6 @@ public class TraineeRegisterManager {
 		System.out.print("학생번호등록>>");
 		String s_num = sc.nextLine();
 		
-		//Lesson abbreviation 보여준다. 과목명요약, 과목명
 		LessonRegisterManager lrm = new LessonRegisterManager();
 		lrm.selectSortManager();
 		System.out.print("과목요약입력>>");
@@ -127,18 +121,18 @@ public class TraineeRegisterManager {
 
 	//전체 학생리스트를 출력진행
 	public static void printTraineeList(ArrayList<TraineeVO> traineeList) {
-		System.out.println("============================================");
+		System.out.println("===========================================================================");
 		for( TraineeVO tvo :  traineeList) {
 			System.out.println(tvo.toString());
 		}
-		System.out.println("============================================");
+		System.out.println("===========================================================================");
 	}
 	//전체 학생리스트를 조인기능을 포함해서 출력진행
 	public static void printTraineeAllList(ArrayList<TraineeVO> traineeList) {
-		System.out.println("============================================");
+		System.out.println("===========================================================================");
 		for( TraineeVO tvo :  traineeList) {
 			System.out.println(tvo.toAllString());
 		}
-		System.out.println("============================================");
+		System.out.println("===========================================================================");
 	}
 }
